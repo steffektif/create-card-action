a4698119a4afea8bd6a6797bdbedef93e65733f6
stefan.herz <stefan.herz@signavio.com>
2019-09-20 15:43:46 +0200

    trying to send a simple http request

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
-fun main(args: Array<String>) {
+import com.github.kittinunf.fuel.Fuel
 
+fun main(args: Array<String>) {
+    Fuel.post("https://webhook.site/6822179a-c255-43cd-bdc3-50be8ca415f6").body("hi!").response()
 }
